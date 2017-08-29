9abd21793e1286898f3b892cb3d32da6a670c1c1
Alexandr Zolotov <zol22222@mail.ru>
2017-08-29 23:14:28 +0700

    Create hosting app

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express'),
+    fs = require('fs'),
+    sites = require('./dust/sites.json'),
+    app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/store'));
+
+app.get('/', (req, res) => {
+    let links = '';
+    for (let key in sites) {
+        links = `
+            <a class="link" href="${sites[key].folder}">
+                <div class="link__container">
+                    <h3 class="link__title">${sites[key].title}</h3>
+                    <p class="link__description">${sites[key].description}</p>
+                </div>
+            </a>
+        `;
+    }
+
+    fs.readFile('dust/index.html', 'utf-8', (error, data) => {
+        if (error) return console.log(error);
+
+        data = data.replace(/{{([^{}]*)}}/g, () => {
+            return links;
+        });
+
+        res.send(data);
+    });
+});
+
+app.use(express.static(__dirname + '/dust'));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
